2ba320a54a0a54e043377f5778313282d8540680
Cody Pizzaia <cpizzaia@asu.edu>
2023-04-28 11:42:49 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PhenixSdk",
-            url: "https://pub-a3c8eff5bc36464d84d7eefe31e3d16f.r2.dev/PhenixSdk.xcframework.zip",
+            url: "https://github.com/cpizzaia/Test/raw/main/PhenixSdk.xcframework.zip",
             checksum: "0dc39103e6cbcdb1c45b161393c54fc2e9282ec06fafe8d951121ed9992a4683"
         ),
     ]
